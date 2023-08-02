85020530d57ae90cf3d8b9c461915c3c9d35695a
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-08-02 20:13:19 +1200

    Add projectile colour changing

--- Rune Casters/Assets/Scripts/Projectile.cs
+++ Rune Casters/Assets/Scripts/Projectile.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,40 @@ public class Projectile : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 startPos;
     public float damage;
+    public Element element = Element.Water;
+    public Color colour;
     public float speed;
     public float range;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        //colour = GetComponent<SpriteRenderer>().color;
+    }
+
+    private void SetColour()
+    {
+        switch (element)
+        {
+            case Element.Basic:
+                colour = Color.black;
+                break;
+            case Element.Fire:
+                colour = Color.red;
+                break;
+            case Element.Earth:
+                colour = Color.magenta;
+                break;
+            case Element.Water:
+                colour = Color.blue;
+                break;
+            case Element.Wind:
+                colour = Color.green;
+                break;
+            default:
+                break;
+        }
+        GetComponent<SpriteRenderer>().color = colour;
     }
 
     public void SetValues()
@@ -20,7 +49,7 @@ public class Projectile : MonoBehaviour
         speed = 10;
         range = 5;
         startPos = transform.position;
-
+        SetColour();
         rb.velocity = transform.position * speed;
     }
 
