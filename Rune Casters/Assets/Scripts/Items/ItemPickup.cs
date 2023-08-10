d1d16a0f79ad80465c9654516d189cefd79aec8c
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-08-10 16:03:00 +1200

    Add item holder

--- Rune Casters/Assets/Scripts/Items/ItemPickup.cs
+++ Rune Casters/Assets/Scripts/Items/ItemPickup.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
 public class ItemPickup : Interactable
 {
     public ItemObject item;
+    public GameObject itemsParent;
 
     private void Start()
     {
         item = GetComponent<ItemObject>();
+        itemsParent = GameObject.Find("ItemsParent");
     }
     public override void Interact()
     {
@@ -20,6 +23,10 @@ public class ItemPickup : Interactable
     void PickUp()
     {
         ItemManager.instance.AddItem(item);
+        transform.SetParent(itemsParent.transform);
+        Destroy(gameObject.GetComponent<Rigidbody2D>());
+        Destroy(gameObject.GetComponent<CircleCollider2D>());
+        Destroy(gameObject.GetComponent<SpriteRenderer>());
         Debug.Log(item.name);
     }
 }
