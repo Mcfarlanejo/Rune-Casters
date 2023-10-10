63e4d957d03d948fb94db66f8fc2a07ebfddfcff
mcfarlanejo <mcfarlanejo1086@gmail.com>
2023-10-10 15:36:45 +1300

    Add 1 of each item need more weapons

--- Rune Casters/Assets/Scripts/Items/ItemPickup.cs
+++ Rune Casters/Assets/Scripts/Items/ItemPickup.cs
@@ -7,12 +7,26 @@ public class ItemPickup : Interactable
 {
     public ItemObject item;
     public GameObject itemsParent;
+    public SpriteRenderer highlight;
+
+    private bool highlightSet = false;
 
     private void Start()
     {
         item = GetComponent<ItemObject>();
         itemsParent = GameObject.Find("ItemsParent");
     }
+
+    private void Update()
+    {
+        if (!highlightSet)
+        {
+            Color temp = UIManager.instance.rarityColours[(int)item.rarity];
+            highlight.color = new Color(temp.r, temp.g, temp.b, 125);
+            highlightSet = true;
+        }
+    }
+
     public override void Interact()
     {
         base.Interact();
@@ -24,6 +38,7 @@ public class ItemPickup : Interactable
     {
         ItemManager.instance.AddItem(item);
         transform.SetParent(itemsParent.transform);
+        Destroy(highlight.gameObject);
         Destroy(gameObject.GetComponent<Rigidbody2D>());
         Destroy(gameObject.GetComponent<CircleCollider2D>());
         Destroy(gameObject.GetComponent<SpriteRenderer>());
